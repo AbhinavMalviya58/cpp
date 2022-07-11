ca5728fc2a10f428226aeeb5de1cb63659f9bbf5
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2022-07-12 01:18:11 +0530

    Add files via upload

---
+++ program to calculate area of a circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    float a;
+    float r;
+    cout<<"Enter radius of a circle"<<endl;
+    cin>>r;
+    a=3.14*r*r;
+    cout<<"airea of a circle is"<<endl<<a;
+    getch();
+}
---
+++ program to calculate simple interest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    float amount,raet,time,si;
+    cout<<"Enter the Amount = ";
+    cin>>amount;
+    cout<<"Enter the Raet = ";
+    cin>>raet;
+    cout<<"Enter the Timr = ";
+    cin>>time;
+    si=amount*raet*time/100;
+    cout<<"Simple interest is = "<<si;
+    getch();
+
+}
