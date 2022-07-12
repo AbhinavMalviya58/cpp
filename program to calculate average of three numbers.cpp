ec553f663a33ff4f30f0be7558db9630a201ecc9
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2022-07-13 01:20:27 +0530

    Add files via upload

---
+++ program to calculate average of three numbers.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+    float A,B,C,sum,avg;
+    cout<<"Enter a number ";
+    cin>>A;
+    cout<<"Enter a number ";
+    cin>>B;
+    cout<<"Enter a number ";
+    cin>>C;
+    sum=A+B+C;
+    avg=sum/3;
+    cout<<"average of three numbers = "<<avg;
+    getch();
+}
